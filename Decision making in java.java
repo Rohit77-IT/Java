2b0badae882a161ac76ab7ee3e49610ff27d217b
Rohit77-IT <dey71393@gmail.com>
2026-02-20 20:05:28 +0530

    Implement comparison method to determine the relationship between two integers

---
+++ Decision making in java.java
@@ -0,0 +1,18 @@
+class Solution
+{
+    public String compareNM(int n, int m)
+    {
+        if (n > m)
+        {
+            return "greater";
+        }
+        else if (n == m)
+        {
+            return "equal";
+        }
+        else
+        {
+            return "lesser";
+        }
+    }
+}
