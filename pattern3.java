2e5da7abd6abcdec14eb89fc0c56b810b5a8df85
Rohit77-IT <dey71393@gmail.com>
2026-08-14 21:24:04 +0530

    Added the 3rd type of pattern in JAVA

---
+++ pattern3.java
@@ -0,0 +1,22 @@
+//Pattern 3:-
+
+//*
+//** n = 4(rows)
+//***
+//****
+
+import java.util.*;
+public class pattern3 {
+    public static void main(String args[]) {
+        int n = 4;
+
+        //outer loop
+        for(int i=1;i<=n;i++) {
+            //inner loop
+            for(int j=1;j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println(); // Move to the next line after each row
+        }
+    }
+}
