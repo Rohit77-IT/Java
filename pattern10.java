ba1d8c468c9ca8c884a321ce7ede837253f7a6ec
Rohit77-IT <dey71393@gmail.com>
2026-08-17 22:00:57 +0530

    Added the 10th type of pattern in JAVA

---
+++ pattern10.java
@@ -0,0 +1,57 @@
+//pattern 10:-
+
+//*      *
+//**    **
+//***  ***
+//********
+//********
+//***  ***
+//**    **
+//*      *
+
+import java.util.*;
+public class pattern10 {
+    public static void main(String args[]) {
+    int n=5;
+
+      //First half - Upper half
+    for(int i=1;i<=n;i++) {
+          //1st part
+        for(int j=1;j<=i;j++) {
+            System.out.print("*");
+        }
+
+          //spaces
+        int spaces = 2*(n-i);
+        for(int j=1;j<=spaces;j++) {
+            System.out.print(" ");
+        }
+
+          //2nd part
+        for(int j=1;j<=i;j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        }
+
+        //lower half
+        for(int i=n;i>=1;i--) {
+          //1st part
+        for(int j=1;j<=i;j++) {
+            System.out.print("*");
+        }
+
+          //spaces
+        int spaces = 2*(n-i);
+        for(int j=1;j<=spaces;j++) {
+            System.out.print(" ");
+        }
+
+          //2nd part
+        for(int j=1;j<=i;j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        }
+    }
+}
